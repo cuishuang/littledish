fbd142dd3e3a61d6e4c9c88424c3a14ebd1c32c1
shuang.cui <i@dashen.tech>
2017-06-26 16:33:19 +0800

    up

---
+++ test10.php
@@ -0,0 +1,21 @@
+<?php
+
+//http://blog.csdn.net/tashanhongye/article/details/47864715
+
+    $var=array('1'=>'A','2'=>'B','3'=>'C','4'=>'D');
+	echo '<pre>';
+    var_export($var);
+	echo '<br>';
+	echo '<br>';
+	echo '<pre>';
+    var_dump($var);
+	
+	
+	
+	echo '<br>';
+	echo '<br>';
+	echo '<br>';
+	echo '<br>';
+	echo '<pre>';
+	
+	print_r($var);
